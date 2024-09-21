493d3565ab7915d18043bd46b1856dc0212691ad
mchizzolmes <matt@wayfoundinc.com>
2024-09-20 22:02:53 -0600

    Added getComputerChoice function.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
 // Computer Tic-Tac-Toe
+
+let computerChoice;
+
+function getComputerChoice () {
+    computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice < 1) computerChoice = "Rock";
+    else if (computerChoice < 2) computerChoice = "Paper";
+    else computerChoice = "Scissors";
+}
+
+getComputerChoice();
+console.log(computerChoice);
