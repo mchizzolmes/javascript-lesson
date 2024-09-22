551e9cca2519db79e1b8781c45974c056a76348e
mchizzolmes <matt@wayfoundinc.com>
2024-09-21 23:27:07 -0600

    Working version 1.0

--- javascript.js
+++ javascript.js
@@ -1,22 +1,50 @@
 // Computer Tic-Tac-Toe
 
+let humanScore = 0;
+let computerScore = 0;
 let humanChoice;
-
-function getHumanChoice () {
-    humanChoice = window.prompt("Rock, Paper, or Scissors?");
-}
-
-getHumanChoice();
-console.log(`You picked: ${humanChoice}`);
-
 let computerChoice;
 
-function getComputerChoice () {
+function getHumanChoice() {
+    humanChoice = window.prompt("Rock, Paper, or Scissors?");
+    humanChoice = humanChoice.toLowerCase();
+    return humanChoice;
+}
+
+function getComputerChoice() {
     computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice < 1) computerChoice = "Rock";
     else if (computerChoice < 2) computerChoice = "Paper";
     else computerChoice = "Scissors";
+    computerChoice = computerChoice.toLowerCase();
+    return computerChoice;
 }
 
-getComputerChoice();
-console.log(computerChoice);
+function playRound(humanChoice, computerChoice) {
+    
+    if (humanChoice == computerChoice) {
+        return;
+    }
+
+    else if ((humanChoice == "scissors" && computerChoice == "paper") || (humanChoice == "paper" && computerChoice =="rock") || (humanChoice == "rock" && computerChoice == "scissors")) {
+        humanScore++;
+        return;
+    }
+
+    else {
+        computerScore++;
+        return;
+    }
+}
+
+function playGame() {
+    for (let i=1; i <= 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        console.log(`You Chose: ${humanSelection}, The Computer Chose: ${computerSelection}`);
+        playRound(humanSelection, computerSelection);
+        console.log(`You: ${humanScore} / Computer: ${computerScore}`);
+    }
+}
+
+playGame();
